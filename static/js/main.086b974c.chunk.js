e947be2919443820438aeaba60c908820c1a55f6
Fattijenishbek <fattijenishbek01@gmail.com>
2020-12-13 14:57:30 +0600

    Updates

---
+++ static/js/main.086b974c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{4:function(e,t,n){"use strict";n.r(t);var p=n(2),o=(n(1),n(3)),r=n.n(o),s=Object(p.jsx)("h1",{children:"Hello World"});r.a.render(s,document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.086b974c.chunk.js.map
